c779b11b48b96fba3487f2d4352bae826265eb1e
denys1953 <den25640@gmail.com>
2020-04-18 20:11:28 +0300

    master

---
+++ main.js
@@ -0,0 +1,37 @@
+var inp = document.querySelector('input');
+let ul = document.querySelector('ul');
+let li = document.querySelector('li');
+inp.addEventListener('keydown', func);
+let item;
+let closed;
+
+document.getElementById('but').onclick = function() {
+   send();
+}
+function func(e) {
+   if (e.keyCode == '13') {
+      send();
+   }
+}
+function send() {
+   if (!inp.value.length) {
+      return;
+   }
+   item = document.createElement('li');
+   closed = document.createElement('button');
+   closed.classList.add('close');
+   closed.id = 'close';
+   closed.innerHTML = '	&#10006;';
+   item.innerHTML = inp.value;
+   inp.value = null;
+   ul.appendChild(item);
+   item.appendChild(closed);
+   let btn = document.getElementsByClassName('close');
+   for (let i = 0; i < btn.length; i++) {
+      btn[i].addEventListener('click', closeg);
+   }
+   function closeg() {
+      this.parentNode.remove();
+   }
+}
+
